1bc2ef0b6549c63b6fa59c7fa981f1ec9c0005fb
Gert Lavsen <gert@houseofcode.io>
2018-09-11 19:33:37 +0200

    removed call to activestorage:install cause it simple doesnt work

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
   Ease the use of active storage as avatar
   This uses ActiveStorage, thus minimum rails version is 5.2
-  
+
 ## Installation
 
 Add this line to your application's Gemfile:
@@ -24,6 +24,14 @@ Or install it yourself as:
 install it with:
 
     $ rails generate acts_as_hoc_avatarable:install
+
+You also need ActiveStorage so run the following
+
+    $ rails activestorage:install
+
+And finally migrate with:
+
+    $ rails db:migrate
 
 ## License
 
--- lib/generators/acts_as_hoc_avatarable/install_generator.rb
+++ lib/generators/acts_as_hoc_avatarable/install_generator.rb
@@ -1,26 +1,16 @@
-require 'rails/generators/migration'
 module ActsAsHocAvatarable
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      include Rails::Generators::Migration
+
       source_root File.expand_path('../templates', __FILE__)
 
       def copy_initializer_file
         template "initializer.rb", "config/initializers/acts_as_hoc_avatarable.rb"
       end
 
-      def install_active_storage
-        rake 'active_storage::install'
-      end
-
-      def self.next_migration_number(dirname)
-        if ActiveRecord::Base.timestamped_migrations
-          sleep 1 # make sure each time we get a different timestamp
-          Time.new.utc.strftime("%Y%m%d%H%M%S")
-        else
-          "%.3d" % (current_migration_number(dirname) + 1)
-        end
-      end
+      # def install_active_storage
+      #   rake 'active_storage::install'
+      # end
     end
   end
 end
