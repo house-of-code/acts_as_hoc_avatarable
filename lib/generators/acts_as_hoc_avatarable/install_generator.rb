89d6c8468a3baa50e6164f3beb733cc82526c397
Gert Lavsen <gert@houseofcode.io>
2018-09-11 18:18:42 +0200

    added missing underscore in string

--- lib/generators/acts_as_hoc_avatarable/install_generator.rb
+++ lib/generators/acts_as_hoc_avatarable/install_generator.rb
@@ -10,7 +10,7 @@ module ActsAsHocAvatarable
       end
 
       def install_active_storage
-        rails_command 'activestorage::install'
+        rails_command 'active_storage::install'
       end
 
       def self.next_migration_number(dirname)
